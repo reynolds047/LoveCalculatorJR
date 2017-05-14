1a4acfe9f3b4289691be5da946881116860bb0ff
reynolds047 <me9945@gmail.com>
2017-05-14 18:44:39 -0400

    finish home.html and home.ts

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,54 +1,36 @@
 import { Component } from '@angular/core';
 import { Contacts } from 'ionic-native';
-import { NavController } from 'ionic-angular';
-
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
-
-  name1: string;
-  name2: string;
-  score: number;
-
-  constructor(public navCtrl: NavController) {
-    this.name1 = "";
-    this.name2 = "";
-  }
-
-  getscore() {
-    const letters = (this.name1 + this.name2).toString().toLowerCase();
+  name1 = '';
+  name2 = '';
+  get score() {
+    const letters = (this.name1 + this.name2).toLowerCase();
     let sum = 0;
     for (let i = 0; i < letters.length; i++) {
       sum += letters.charCodeAt(i);
-      console.log("sum inside loop" + sum);
     }
-
-    console.log("sum" + sum);
-    this.score = sum % 101;
-
+    return sum % 101;
   }
-
-
-  pickContact1() {
+  picContact1() {
     Contacts.pickContact()
       .then(contact => {
         this.name1 = contact.displayName;
       })
       .catch(error => {
-       // ignore; user didn't select a contact
+        // ignore; user didn't select a contact
       });
   }
-
-  pickContact2() {
+  picContact2() {
     Contacts.pickContact()
       .then(contact => {
         this.name2 = contact.displayName;
       })
       .catch(error => {
-      // ignore; user didn't select a contact
+        // ignore; user didn't select a contact
       });
   }
-
 }
